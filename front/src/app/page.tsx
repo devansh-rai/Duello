639a10e11d8b493e9dbaa1edb424e5424238b970
devansh-rai <raianupam2002@gmail.com>
2025-11-15 21:48:25 +0530

    Initial Project Setup

---
+++ front/src/app/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <div className='bg-red-200 h-screen'>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <br></br>
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default App
